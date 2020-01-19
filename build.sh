bd038298a6a0e0255f30dc6d42b9514c65a28f59
James Patillo <james.patillo@gmail.com>
2020-01-19 12:48:48 -0500

    more streamlined building

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-# Mosquitto
+# Get system updates
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install Mosquitto
 sudo apt-get install libmosquitto-dev libmosquittopp-dev libssl-dev -y
+
+# Shutdown and cleanup old installation
+systemctl stop noodle
+rm -r build
 
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=debug -DDAEMON=OFF ../
+cmake -DCMAKE_BUILD_TYPE=Release -DDAEMON=ON ../
 make noodle
+make install
+
+systemctl start noodle
+systemctl enable noodle
