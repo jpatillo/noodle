ee07ac233c2267f2343734f98e6b441d702aa62b
James Patillo <james.patillo@gmail.com>
2020-01-19 12:43:31 -0500

    simplified resources

--- source/res/integers.h
+++ source/res/integers.h
@@ -1,37 +1,18 @@
 #ifndef NOODLE_RES_INTEGERS_H_
 #define NOODLE_RES_INTEGERS_H_
 
-// Max db rows that can be sent to the web service at one time
-const unsigned int WEB_SYNC_ROW_LIMIT = 5;
-// Max messages that can be handled per thread loop
-const unsigned int WEB_SYNC_MSG_LIMIT = 5;
 // Time between sensor checks
 const unsigned int DEFAULT_SENSOR_INTERVAL = 300; // 5 minutes
 const unsigned int MAX_SENSOR_INTERVAL = 86400; // 1 day
+const unsigned int MIN_SENSOR_INTERVAL = 15; // 15 seconds
 
-// Time between checking for currently connected sensors
+// Time between checking for currently mounted sensors
 const unsigned int DEFAULT_MOUNT_INTERVAL = 600; // 10 minutes
 const unsigned int MAX_MOUNT_INTERVAL = 86400; // 1 day
+const unsigned int MIN_MOUNT_INTERVAL = 300; //5 minutes
 
-// These keys will be used as ContentValues keys for identifying data quickly.
-const unsigned int KEY_DEVICEID = 0x01;
-const unsigned int KEY_MODEL = 0x02;
-const unsigned int KEY_MANUFACTURED = 0x03;
-const unsigned int KEY_FIRMWARE = 0x04;
-const unsigned int KEY_SENSORID = 0x05;
-const unsigned int KEY_SENSORTYPE = 0x06;
-const unsigned int KEY_DATA = 0x07;
-const unsigned int KEY_TEMPERATURE = 0x08;
-const unsigned int KEY_CREATED = 0x09;
-const unsigned int KEY_SENSORS = 0x0A;
-
-const unsigned int DHT11_FAMILY = 0xD1;
-const unsigned int DHT22_FAMILY = 0xD2;
-
-// 1 byte
-const unsigned int PROTOCOL_GENERIC = 0x00;
-const unsigned int PROTOCOL_1WIRE = 0x01;
-const unsigned int PROTOCOL_I2C = 0x04;
-const unsigned int PROTOCOL_SPI = 0x08;
+// Thermostat defaults.
+const unsigned int DEFAULT_MAX_TEMP = 29444; //85F
+const unsigned int DEFAULT_MIN_TEMP = 25555; //78F
 
 #endif
--- source/res/strings.h
+++ source/res/strings.h
@@ -8,8 +8,7 @@ static std::string TOPIC_DEVICE = "/device";
 // Add all keys from the config file here for easy access
 static std::string CHECK_SENSORS_INTERVAL = "CHECK_SENSORS_INTERVAL";
 static std::string CHECK_MOUNT_INTERVAL = "CHECK_MOUNT_INTERVAL";
-// Static keys
-static std::string DEVICE_MODEL = "DEVICE_MODEL";
-static std::string MANUFACTURE_DATE = "MANUFACTURE_DATE";
 
+
+static std::string DEVICE_MODEL_PATH = "/sys/firmware/devicetree/base/model";
 #endif
