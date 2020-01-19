543b91575d355b2f3640ed8e4350f3a84cacac1f
James Patillo <james.patillo@gmail.com>
2020-01-19 12:45:06 -0500

    relay state

--- source/relay.h
+++ source/relay.h
@@ -16,13 +16,15 @@ public:
     relay(unsigned int pin){
         set_pin(pin);
     }
-    ~relay(){}
+    ~relay(){off();}
     void set_pin(unsigned int pin){
         _pin = pin;
         pinMode( pin, OUTPUT );
+        off();
     }
     void on(){digitalWrite( _pin, LOW );}
     void off(){digitalWrite( _pin, HIGH );}
+    bool isOn(){return digitalRead(_pin)==LOW;}
 };
 
 #endif
