7ed471ae65e0ea00a26fcc78b99c750b63ce6fb2
James Patillo <james.patillo@gmail.com>
2020-11-12 00:16:57 -0500

    update static resources

--- source/res/integers.h
+++ source/res/integers.h
@@ -11,4 +11,6 @@ const unsigned int DEFAULT_MOUNT_INTERVAL = 600; // 10 minutes
 const unsigned int MAX_MOUNT_INTERVAL = 86400; // 1 day
 const unsigned int MIN_MOUNT_INTERVAL = 300; //5 minutes
 
+const unsigned int SLEEP_INTERVAL = 30; // 30 second sleep after each loop
+
 #endif
--- source/res/strings.h
+++ source/res/strings.h
@@ -6,9 +6,12 @@ static std::string TOPIC_TELEMETRY = "/telemetry";
 static std::string TOPIC_SENSOR = "/sensor";
 static std::string TOPIC_DEVICE = "/device";
 // Add all keys from the config file here for easy access
-static std::string CHECK_SENSORS_INTERVAL = "CHECK_SENSORS_INTERVAL";
-static std::string CHECK_MOUNT_INTERVAL = "CHECK_MOUNT_INTERVAL";
+static std::string CHECK_SENSORS_INTERVAL_KEY = "CHECK_SENSORS_INTERVAL";
+static std::string CHECK_MOUNT_INTERVAL_KEY = "CHECK_MOUNT_INTERVAL";
 
 
 static std::string DEVICE_MODEL_PATH = "/sys/firmware/devicetree/base/model";
+
+static std::string DAEMON_NAME = "noodle";
+
 #endif
